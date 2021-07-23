a3b8d3ce48673e2edde7ce290fb183a0a07de9b9
Mikhail Sechin <mikhailsechin@gmail.com>
2021-07-23 13:31:51 +0300

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
+// start server
 const app = express();
+
+// connect DB
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`[log] Server started on port ${PORT}`));
