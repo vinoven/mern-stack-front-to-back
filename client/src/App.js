f5799ca304adca6d9235cf80b086c295aeebf5d5
Mikhail Sechin <mikhailsechin@gmail.com>
2021-07-28 14:04:47 +0300

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Landing />
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/Login' component={Login} />
+          </Switch>
+        </section>
+      </>
+    </Router>
   );
 };
 
