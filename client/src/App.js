06db13a80234c422d288a9be2153d2cd19a89185
Mikhail Sechin <mikhailsechin@gmail.com>
2021-07-28 13:28:39 +0300

    Add components for basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 const App = () => {
-  return <>App</>;
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
 };
 
 export default App;
